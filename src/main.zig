d8a3e5b799d4b78f87773e90e757c154510e3e8f
Mick Sayson <mick@sayson.com>
2024-12-20 12:32:12 -0800

    :)

--- src/main.zig
+++ src/main.zig
@@ -144,36 +144,110 @@ const LspReferenceRetreiver = struct {
     }
 };
 
+const Config = struct {
+    project_root: []const u8,
+    language_server: []const []const u8,
+    language_id: []const u8,
+};
+
+const Args = struct {
+    config: []const u8,
+    file: []const u8,
+    line: u32,
+    col: u32,
+    it: std.process.ArgIterator,
+
+    fn deinit(self: *Args) void {
+        self.it.deinit();
+    }
+
+    fn parse(alloc: Allocator) Args {
+        var it = try std.process.argsWithAllocator(alloc);
+
+        const process_name = it.next() orelse "code-map";
+
+        const config = nextArg(&it, "config", process_name);
+        const file = nextArg(&it, "file", process_name);
+
+        const line_s = nextArg(&it, "line", process_name);
+        const line = parseInt(u32, line_s, "line", process_name);
+
+        const col_s = nextArg(&it, "col", process_name);
+        const col = parseInt(u32, col_s, "col", process_name);
+
+        return .{
+            .config = config,
+            .file = file,
+            .line = line,
+            .col = col,
+            .it = it,
+        };
+    }
+
+    fn nextArg(it: *std.process.ArgIterator, comptime field: []const u8, process_name: []const u8) []const u8 {
+        const val = it.next() orelse {
+            std.log.err("No " ++ field ++ " provided", .{});
+            help(process_name);
+        };
+
+        if (std.mem.eql(u8, val, "--help")) {
+            help(process_name);
+        }
+
+        return val;
+    }
+
+    fn parseInt(comptime T: type, s: []const u8, comptime field: []const u8, process_name: []const u8) T {
+        return std.fmt.parseInt(u32, s, 0) catch {
+            std.log.err("Invalid " ++ field, .{});
+            help(process_name);
+        };
+    }
+
+    fn help(program_name: []const u8) noreturn {
+        const stderr = std.io.getStdErr().writer();
+        stderr.print(
+            \\USAGE: {s} <config> <file> <line> <col>
+            \\
+            \\Print references at file/line/col using config
+        , .{program_name}) catch {};
+
+        std.process.exit(1);
+    }
+};
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
 
     const alloc = gpa.allocator();
 
-    const args = try std.process.argsAlloc(alloc);
-    defer std.process.argsFree(alloc, args);
+    var args = Args.parse(alloc);
+    defer args.deinit();
 
-    const project_dir = args[1];
-    const file = args[2];
-    const line = try std.fmt.parseInt(u32, args[3], 0);
-    const col = try std.fmt.parseInt(u32, args[4], 0);
+    const config_f = try std.fs.cwd().openFile(args.config, .{});
+    var config_reader = std.json.reader(alloc, config_f.reader());
+    defer config_reader.deinit();
 
-    const abs_project_dir = try std.fs.cwd().realpathAlloc(alloc, project_dir);
+    const config = try std.json.parseFromTokenSource(Config, alloc, &config_reader, .{});
+    defer config.deinit();
+
+    const abs_project_dir = try std.fs.cwd().realpathAlloc(alloc, config.value.project_root);
     defer alloc.free(abs_project_dir);
 
-    var retriever = try LspReferenceRetreiver.init(alloc, &.{"zls"}, abs_project_dir, "zig");
+    var retriever = try LspReferenceRetreiver.init(alloc, config.value.language_server, abs_project_dir, config.value.language_id);
     defer retriever.deinit();
 
     var arena = std.heap.ArenaAllocator.init(alloc);
     defer arena.deinit();
 
-    const abs_file = try std.fs.cwd().realpathAlloc(alloc, file);
+    const abs_file = try std.fs.cwd().realpathAlloc(alloc, args.file);
     defer alloc.free(abs_file);
 
     for (0..3) |_| {
-        const references = try retriever.findReferencesLeaky(arena.allocator(), abs_file, line, col);
+        const references = try retriever.findReferencesLeaky(arena.allocator(), abs_file, args.line, args.col);
         for (references) |loc| {
-            std.debug.print("ref: {s} ({d}, {d})\n", .{loc.uri, loc.range.start.line, loc.range.start.character});
+            std.debug.print("ref: {s} ({d}, {d})\n", .{ loc.uri, loc.range.start.line, loc.range.start.character });
         }
         _ = arena.reset(.retain_capacity);
     }
